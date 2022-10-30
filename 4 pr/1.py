a654c219ee9f3f36c98c1ee816b59910cfd84f70
INTERFEJS <vitac882@gmail.com>
2022-10-31 02:34:24 +0300

    pr_4

--- 4 pr/1.py
+++ 4 pr/1.py
@@ -1,9 +1,9 @@
 # -- coding: utf-8 --
-A = int(input("Введите A: "))
-B = int(input("Введите B (больше или равно А): "))
-C = B + 1
-if A <= B:
-    for i in range (A, C):
-        print(i, end = "; ")
-else:
-    print("B меньше А")
+def func(A, B):
+    C = B + 1
+    if A <= B:
+        for i in range (A, C):
+            print(i, end = "; ")
+    else:
+        print("B меньше А")
+func(int(input("Введите A: ")), int(input("Введите B (больше или равно А): ")))
