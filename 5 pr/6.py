361ba61be687a951f1dc071c48bfb79c710582f2
INTERFEJS <vitac882@gmail.com>
2022-11-02 19:14:38 +0300

    N_pr5

--- 5 pr/6.py
+++ 5 pr/6.py
@@ -7,5 +7,5 @@ def func(s):
         i += 1
         s = int(input("Введите число: "))
         lst.append(s)
-    print((sum(lst))/(i+1))
+    print((sum(lst))/(i+1)) # С учетом 0
 func(int(input("Введите число: ")))
