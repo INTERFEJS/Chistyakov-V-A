408375fcccb1cbc1e1a509337e805eba24561e30
INTERFEJS <vitac882@gmail.com>
2022-11-06 01:55:39 +0300

    pr_7

--- 7 pr/1.py
+++ 7 pr/1.py
@@ -7,5 +7,18 @@ def func(N):
         N = int(input("Введите число: "))
         if N != 0:
             lst.append(N)
-    print(lst.index(max(lst)))
+    print("Исходный список:", lst)
+    max_count = max(lst)
+    min_count = min(lst)
+    max_index = lst.index(max(lst))
+    min_index = lst.index(min(lst))
+    lst.insert(min_index, max_count)
+    if max_index >= min_index:    
+        lst.pop(max_index+1)
+        lst.insert(max_index+1, min_count)
+    else:
+        lst.pop(max_index)
+        lst.insert(max_index, min_count)
+    lst.pop(min_index+1)
+    print("Изменённый список:", lst)
 func(int(input("Введите число (чтобы закончить ввод введите 0): ")))
